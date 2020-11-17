bed23e8f3dd9fa6a47521ab1a234938fbdf121d8
Shinoj Lalgi <69868383+ShinojLalgi@users.noreply.github.com>
2020-11-17 12:32:21 +0530

    Create ticket.py

---
+++ ticket.py
@@ -0,0 +1,24 @@
+name = []
+seat_num = []
+ask_name = ""
+ask_seat = ""
+while True:
+    ask_name = input("Please enter your name: ")
+    if ask_name != "exit":
+        ask_seat = input("Please enter your seat number: ")
+        if ask_seat == "exit":
+            print("Thank you for using our services.")
+            break
+        elif ask_name not in name and ask_seat not in seat_num:
+            name.append(ask_name)
+            seat_num.append(int(ask_seat))
+            print(f"{ask_name.title()} your seat number is {int(ask_seat)}.")
+        else:
+            if ask_seat in seat_num:
+                print(f"Sorry {ask_name.title()} your desired seat is already booked")
+            else:
+                print(f"Sorry {ask_name.title()} you have already booked a seat")
+    else:
+        print("Thank you for using our services.")
+        break
+    
